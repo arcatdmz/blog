83aba06879ab6c710125b1458fea4346314ae579
Jun Kato <i@junkato.jp>
2021-03-21 19:06:52 +0900

    Working on a brand-new blog based on: https://github.com/timlrx/tailwind-nextjs-starter-blog

---
+++ pages/_app.tsx
@@ -0,0 +1,21 @@
+import { MDXProvider } from "@mdx-js/react";
+// import { DefaultSeo } from "next-seo";
+import Head from "next/head";
+
+// import { SEO } from "../components/SEO";
+import LayoutWrapper from "../components/LayoutWrapper";
+import MDXComponents from "../components/MDXComponents";
+
+export default function App({ Component, pageProps }) {
+  return (
+    <MDXProvider components={MDXComponents}>
+      <Head>
+        <meta content="width=device-width, initial-scale=1" name="viewport" />
+      </Head>
+      {/* <DefaultSeo {...SEO} /> */}
+      <LayoutWrapper>
+        <Component {...pageProps} />
+      </LayoutWrapper>
+    </MDXProvider>
+  );
+}
