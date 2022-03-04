3b96e7b5c0fe436cf4b7dca079113e005ab3f4bf
Jun Kato <i@junkato.jp>
2022-03-05 00:51:09 +0900

    Minor fix

--- lib/generate-rss.ts
+++ lib/generate-rss.ts
@@ -31,26 +31,25 @@ const generateRss = (
   if (!language) {
     language = "default";
   }
-  return `
-  <rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
-    <channel>
-      <title>${websiteJson.languages[language].title}</title>
-      <link>${websiteJson.languages[language].siteUrl}</link>
-      <description>${websiteJson.languages[language].description}</description>
-      <language>${websiteJson.languages[language].locale}</language>
-      <managingEditor>${websiteJson.languages[language].email} (${
+  return `<rss version="2.0" xmlns:atom="http://www.w3.org/2005/Atom">
+  <channel>
+    <title>${websiteJson.languages[language].title}</title>
+    <link>${websiteJson.languages[language].siteUrl}</link>
+    <description>${websiteJson.languages[language].description}</description>
+    <language>${websiteJson.languages[language].locale}</language>
+    <managingEditor>${websiteJson.languages[language].email} (${
     websiteJson.languages[language].author
   })</managingEditor>
-      <webMaster>${websiteJson.languages[language].email} (${
+    <webMaster>${websiteJson.languages[language].email} (${
     websiteJson.languages[language].author
   })</webMaster>
-      <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
-      <atom:link href="${
-        websiteJson.languages[language].siteUrl
-      }${path}" rel="self" type="application/rss+xml"/>
-      ${posts.map(post => generateRssItem(post, language)).join("")}
-    </channel>
-  </rss>
+    <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
+    <atom:link href="${
+      websiteJson.languages[language].siteUrl
+    }${path}" rel="self" type="application/rss+xml"/>
+    ${posts.map(post => generateRssItem(post, language)).join("")}
+  </channel>
+</rss>
 `;
 };
 
