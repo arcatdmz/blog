1e5f5d7a79db8fd2033f3b63f5f7141d8c82b856
Jun Kato <i@junkato.jp>
2022-11-18 01:50:52 +0900

    Fix #7

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,7 +1,9 @@
 import Document, { Html, Head, Main, NextScript } from "next/document";
 import { GA_TRACKING_ID } from "../lib/useGoogleAnalytics";
 
-const __html = `window.dataLayer = window.dataLayer || [];
+const __ahtml = `(function(d){var config={kitId:"tor0txx",scriptTimeout:3000,async:true},h=d.documentElement,t=setTimeout(function(){h.className=h.className.replace(/\bwf-loading\b/g,"")+" wf-inactive";},config.scriptTimeout),tk=d.createElement("script"),f=false,s=d.getElementsByTagName("script")[0],a;h.className+=" wf-loading";tk.src='https://use.typekit.net/'+config.kitId+'.js';tk.async=true;tk.onload=tk.onreadystatechange=function(){a=this.readyState;if(f||a&&a!="complete"&&a!="loaded")return;f=true;clearTimeout(t);try{Typekit.load(config)}catch(e){}};s.parentNode.insertBefore(tk,s)})(document);`;
+
+const __ghtml = `window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
 gtag('config', '${GA_TRACKING_ID}', {
@@ -15,16 +17,17 @@ class MyDocument extends Document {
         <Head>
           <link rel="stylesheet" href="/stylesheets/main.css" />
           <script
-            type="text/javascript"
-            src="//webfonts.sakura.ne.jp/js/sakurav3.js"
-          ></script>
+            dangerouslySetInnerHTML={{
+              __html: __ahtml
+            }}
+          />
           <script
             async
             src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
           />
           <script
             dangerouslySetInnerHTML={{
-              __html
+              __html: __ghtml
             }}
           />
         </Head>
