12b4c9a3df7481264d0cd72ca1ede64607f3114f
Jun Kato <i@junkato.jp>
2023-06-17 18:48:20 +0000

    Update img element styling in posts

--- components/MDXComponents.tsx
+++ components/MDXComponents.tsx
@@ -1,10 +1,16 @@
-import { FC } from "react";
+import { DetailedHTMLProps, FC, ImgHTMLAttributes } from "react";
 import { Divider, Image } from "semantic-ui-react";
 
 import { CustomLink } from "./CustomLink";
 
 const MDXComponents = {
-  img: (props => <Image {...props} fluid />) as FC<HTMLImageElement>,
+  /* this doesn't work in some cases */
+  img: (props => (
+    <Image {...props} fluid />
+  )) /* <img {...props} className="ui fluid image" /> */ as FC<
+    DetailedHTMLProps<ImgHTMLAttributes<HTMLImageElement>, HTMLImageElement>
+  >,
+
   a: CustomLink,
   hr: Divider
 };
