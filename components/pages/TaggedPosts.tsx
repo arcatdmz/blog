698892bb8d39f3efd5007b3264b66e82c52a7200
Jun Kato <i@junkato.jp>
2025-12-27 03:34:22 +0900

    Remove obsolete SEO component

--- components/pages/TaggedPosts.tsx
+++ components/pages/TaggedPosts.tsx
@@ -4,7 +4,6 @@ import { FC, useContext } from "react";
 import { BlogContext } from "../../lib/BlogContext";
 import { PostIface } from "../../lib/PostIface";
 
-import { PageSeo } from "../SEO";
 import { BaseLayout } from "../layouts/BaseLayout";
 import { ListLayout } from "../layouts/ListLayout";
 
@@ -14,17 +13,12 @@ interface PostsProps {
 }
 
 const TaggedPosts: FC<PostsProps> = ({ posts, tag }) => {
-  const blog = useContext(BlogContext);
+  const { language } = useContext(BlogContext);
   const text =
-    blog.language === "ja" ? "タグ付けされた投稿: " : "Posts tagged with ";
+    language === "ja" ? "タグ付けされた投稿: " : "Posts tagged with ";
 
   return (
     <BaseLayout>
-      <PageSeo
-        title={`${text}"${tag}" | ${blog.title}`}
-        description={blog.description}
-        url={blog.siteUrl}
-      />
       <ListLayout posts={posts} title={`${text}"${tag}"`} />
     </BaseLayout>
   );
