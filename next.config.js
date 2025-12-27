a93bde7927b5c3ea2dca868a6ce12fc3aaedc7e6
Jun Kato <i@junkato.jp>
2025-12-27 13:08:52 +0900

    Simplify Next.js config

--- next.config.js
+++ next.config.js
@@ -3,23 +3,8 @@
  */
 const nextConfig = {
   output: "export",
-  pageExtensions: ["js", "jsx", "ts", "tsx", "md", "mdx"],
-  trailingSlash: true,
-  webpack: config => {
-    config.module.rules.push({
-      test: /\.(png|jpe?g|gif|mp4)$/i,
-      use: [
-        {
-          loader: "file-loader",
-          options: {
-            publicPath: "/_next",
-            name: "static/media/[name].[hash].[ext]"
-          }
-        }
-      ]
-    });
-    return config;
-  }
+  pageExtensions: ["tsx", "md"],
+  trailingSlash: true
 };
 
 module.exports = nextConfig;
