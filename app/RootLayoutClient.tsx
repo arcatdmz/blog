3db34f6745b55476146a69df85cdd60380169cb1
Jun Kato <i@junkato.jp>
2025-12-28 17:41:24 +0900

    Remove X embedding script (goes wrong with React)

--- app/RootLayoutClient.tsx
+++ app/RootLayoutClient.tsx
@@ -26,12 +26,6 @@ export function RootLayoutClient({ children }: { children: ReactNode }) {
           __html: `window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','${GA_TRACKING_ID}');`
         }}
       />
-      <Script
-        id="twitter"
-        dangerouslySetInnerHTML={{
-          __html: `window.twttr=(function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],t=window.twttr||{};if(d.getElementById(id))return t;js=d.createElement(s);js.id=id;js.src="https://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);t._e=[];t.ready=function(f){t._e.push(f);};return t;}(document,"script","twitter-wjs"));`
-        }}
-      />
       {children}
     </>
   );
